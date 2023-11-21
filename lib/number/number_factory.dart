4ca3cf667a47a57f9ee5c32fd17683cfd4fc91fe
Thorsten Jahrsetz <thorsten.jahrsetz@noreply.noreply>
2023-11-21 22:54:06 +0100

    Refactored float parsing

--- lib/number/number_factory.dart
+++ lib/number/number_factory.dart
@@ -23,18 +23,26 @@ class NumberFactory {
   final decimalPoint = '.';
 
   Number parseFloat(List<ParseTree> children, int childNumber) {
-    final indexDecimalPoint = children.indexWhere((element) => element.text == decimalPoint);
-    final indexExponentSeparator = children.indexWhere((element) => element.text == exponentSeparator);
     final base = int.parse(children[childNumber - 1].text!);
+    double number = _getValue(children, base);
+    double exponentialPart = _getExponentialPart(children, childNumber, base);
+
+    return FloatNumber(number * exponentialPart);
+  }
+
+  double _getExponentialPart(List<ParseTree> children, int childNumber, int base) {
+    final indexExponentSeparator = children.indexWhere((element) => element.text == exponentSeparator);
+    final exponentElements = children.sublist(indexExponentSeparator + 1, childNumber - 2);
+    final exponentValue = NumberParser.parseInt(exponentElements.last.text!, base);
+    final exponentSign = exponentElements.length == 2 ? (exponentElements[0].text == '-' ? -1 : 1) : 1;
+    return pow(base.toDouble(), exponentValue * exponentSign).toDouble();
+  }
+
+  double _getValue(List<ParseTree> children, int base) {
+    final indexDecimalPoint = children.indexWhere((element) => element.text == decimalPoint);
     final numberBeforeDecimalSeparator = NumberParser.parseInt(children[indexDecimalPoint - 1].text!, base);
     final numberAfterDecimalSeparator =
         NumberParser.parsePostDecimalPointPart(children[indexDecimalPoint + 1].text!, base);
-
-    final exponentElements = children.sublist(indexExponentSeparator+1, childNumber - 2);
-    final exponentValue = NumberParser.parseInt(exponentElements.last.text!, base);
-    final exponentSign = exponentElements.length == 2 ? (exponentElements[0].text == '-' ? -1 : 1) : 1;
-    final exponent = pow(base.toDouble(), exponentValue * exponentSign);
-
-    return FloatNumber((numberBeforeDecimalSeparator + numberAfterDecimalSeparator) * exponent);
+    return numberBeforeDecimalSeparator + numberAfterDecimalSeparator;
   }
 }
